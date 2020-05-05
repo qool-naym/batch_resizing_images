63cab5fba7634f342afc54be51b1bf73738547cc
qool-naym <56560993+qool-naym@users.noreply.github.com>
2020-05-05 14:55:24 +0200

    Update img_batch_res.py

--- img_batch_res.py
+++ img_batch_res.py
@@ -5,26 +5,37 @@ import os
 import time
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-d", "--dimensions", default=512, help="")
-parser.add_argument("-fn", "--filename", default="image-", help="")
+parser.add_argument("-d", "--dimensions", default=512, help="Images will be resized to 512x512 by default.")
+parser.add_argument("-fn", "--filename", default="image-")
 args = parser.parse_args()
 
 start_time = time.clock()
 m_path = os.path.abspath(".")
-s_path = m_path+"/source_folder/*"
-d_path = m_path+"/destination_folder/"
+s_path = m_path+"/*"
+d_path = m_path+"/resized"
+
+is_file = os.path.isfile(d_path)
+
+if is_file == True:
+    os.mkdir(d_path)
+elif is_file == False:
+    pass
+
 filename = str(args.filename)
 d_width = d_height = int(args.dimensions) 
 counter = 0
 
 for image in glob.glob(s_path):
-    counter += 1
-    img = cv2.imread(image, cv2.IMREAD_UNCHANGED)
-    dim = (d_width, d_height)
-    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-    img_count = str(counter).zfill(4)
-    cv2.imwrite((d_path+filename+str(img_count)+".jpg"), resized)
-    print(str(img_count)) 
+    if image.endswith(".jpg") or image.endswith(".png"):
+        counter += 1
+        img = cv2.imread(image, cv2.IMREAD_UNCHANGED)
+        dim = (d_width, d_height)
+        resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+        img_count = str(counter).zfill(5)
+        cv2.imwrite((d_path+"/"+filename+str(img_count)+".jpg"), resized)
+        print(str(img_count)) 
+    else:
+        pass
 
 print("\n")
 print("Processing time:")
